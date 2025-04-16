8362cd6b860dd0dbc2a30474a653c88c22d63e9b
Ville <dingowar99@gmail.com>
2025-04-16 12:43:46 +0300

    wait fix

--- base/base_page.py
+++ base/base_page.py
@@ -7,7 +7,7 @@ class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 15, poll_frequency=1)
 
     def open(self):
         with allure.step(f"Open {self.PAGE_URL} page"):
