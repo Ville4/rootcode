f9cbc9a4398d43a20503f8500ec186edf1123238
Ville <dingowar99@gmail.com>
2025-04-16 12:16:11 +0300

    minor fix2

--- pages/home_page.py
+++ pages/home_page.py
@@ -8,7 +8,7 @@ class HomePage(BasePage):
 
     PAGE_URL = Links.HOST
 
-    COOPERATION_LINK = ("xpath", "//a[@class = 'text-link-adaptive p1' and text()='Сотрудничество']")
+    COOPERATION_LINK = ("xpath", "//li[a[@class = 'text-link-adaptive p1' and text()='Сотрудничество']]")
 
     @allure.step("Click on cooperation link")
     def click_cooperation_link(self):
